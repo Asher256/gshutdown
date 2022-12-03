0496934d908f955cc323f326bc298fff7e963d8f
Asher256 <Asher256@users.noreply.github.com>
2022-12-03 08:37:06 -0500

    Update AUTHORS and add issues URL.

--- src/action.c
+++ src/action.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of gshutdown.
  *
- * Copyright (c) GShutdown Team <gshutdown@gmail.com>
+ * Copyright (c) GShutdown Team <https://github.com/Asher256/gshutdown/issues>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
